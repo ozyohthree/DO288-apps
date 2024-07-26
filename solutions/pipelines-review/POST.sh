86f3ccab2365e29def0e97c5f040a3bac0857866
Anthony G. <ozyohthree@gmail.com>
2024-07-26 11:22:50 -0400

    updates

--- solutions/pipelines-review/POST.sh
+++ solutions/pipelines-review/POST.sh
@@ -1,4 +1,4 @@
 curl -H 'Content-Type: application/json' \
-     -d '{"head_commit":{"id":"pipelines-review-webhook"},"repository":{"url":"https://github.com/ozyohthree/DO288-apps","name": "apps/pipelines-review/vertx-site"}}' \
+     -d '{"head_commit":{"id":"pipelines-review-soln"},"repository":{"url":"https://github.com/ozyohthree/DO288-apps","name": "apps/pipelines-review/vertx-site"}}' \
      -X POST \
      http://el-maven-java-pipeline-pipelines-review.apps.ocp4.example.com
--- solutions/pipelines-review/run.sh
+++ solutions/pipelines-review/run.sh
@@ -2,7 +2,7 @@ tkn p start maven-java-pipeline \
   -w name=shared,volumeClaimTemplateFile=volume-template.yaml \
   -w name=maven_config,secret=mvn-settings \
   -p GIT_REPO="https://github.com/ozyohthree/DO288-apps" \
-  -p GIT_REVISION="pipelines-review-webhook" \
+  -p GIT_REVISION="pipelines-review-soln" \
   -p MVN_APP_PATH="apps/pipelines-review/vertx-site" \
   -p DEPLOY_ARTIFACT_NAME="target/vertx-site-1.0.0-SNAPSHOT-fat.jar" \
   -p DEPLOY_APP_NAME="vertx-site" \
