3d2db7816f9445a28563d01fe98f100cbdcbba9a
Anthony G. <ozyohthree@gmail.com>
2024-07-29 13:29:06 -0400

    updates

--- solutions/pipelines-review/POST.sh
+++ solutions/pipelines-review/POST.sh
@@ -1,12 +1,12 @@
 curl -H 'Content-Type: application/json'                          \
-     -d '{                                                        \
-            "head_commit": {                                      \
-              "id": "pipelines-review-soln"                       \
-            },                                                    \
-            "repository": {                                       \
-              "name": "apps/pipelines-review/vertx-site",         \
-              "url": "https://github.com/ozyohthree/DO288-apps"   \
-            }                                                     \
+     -d '{                                                        
+            "head_commit": {                                      
+              "id": "pipelines-review-soln"                       
+            },                                                    
+            "repository": {                                       
+              "name": "apps/pipelines-review/vertx-site",         
+              "url": "https://github.com/ozyohthree/DO288-apps"   
+            }                                                     
           }'                                                      \
      -X POST http://el-maven-java-pipeline-pipelines-review.apps.ocp4.example.com
 
